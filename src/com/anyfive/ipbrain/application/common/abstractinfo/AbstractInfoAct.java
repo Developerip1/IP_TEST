ac64a4b8ac81df134fcd45d9dcda6fbd27fcc8c5
Developer <jinkyu6.lee@doosan.com>
2024-08-28 14:19:31 +0900

    최초세팅

---
+++ src/com/anyfive/ipbrain/application/common/abstractinfo/AbstractInfoAct.java
@@ -0,0 +1,23 @@
+package com.anyfive.ipbrain.application.common.abstractinfo;
+
+import java.util.Iterator;
+
+/**
+ * 서지정보 Act
+ */
+public class AbstractInfoAct
+{
+    /**
+     * 조회
+     *
+     * @param req
+     * @param res
+     * @param nsr
+     */
+    public void retrieve()
+    {
+      String test = "1";
+
+      System.out.println(test);
+    }
+}
